693142320b3aa0fb031abfd936b05c47091f4380
dewang1 <derek.zmw@gmail.com>
2024-05-30 16:58:36 -0400

    edited maps

---
+++ tiles/5_waterfall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="5_waterfall" tilewidth="16" tileheight="16" tilecount="360" columns="18">
+ <image source="5_waterfall.png" width="288" height="320"/>
+</tileset>
